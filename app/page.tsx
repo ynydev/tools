718223e4d0b5769922c889091736cf392de6dc93
ゆむ <ynto.dev@proton.me>
2025-11-09 14:51:06 +0000

    今更

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import tools from "@/data/tools.json";
 export default function Home() {
   return (
     <div className="px-10 space-y-6 sm:px-20 lg:px-30">
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 pb-10">
         {tools.map((tool) => (
           <ToolCard key={tool.href} {...tool} />
         ))}
